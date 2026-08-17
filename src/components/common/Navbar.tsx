719cbf878d6e0361f6bc65b811472d6dfdeb7d49
ardayrkmm <ardyrkm23@gmail.com>
2026-08-18 06:12:17 +0700

    update

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -56,6 +56,9 @@ export const Navbar = () => {
           );
         })}
       </div>
+      <div className={styles.mobileMenuBtn}>
+        <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" strokeWidth="2" fill="none"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
+      </div>
     </nav>
   );
 };
